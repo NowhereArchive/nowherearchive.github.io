96ff2029f42707e4297266eae1ec7d8435a86871
JSleim <122807475+JSleim@users.noreply.github.com>
2026-05-31 20:19:39 +0300

    Updates

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,10 @@
 export const ASSET_BASE_URL = 'https://raw.githubusercontent.com/NowhereArchive';
-export const ASSET_AUDIO_URL = `${ASSET_BASE_URL}/NowhereAudios/main/audio`;
+export const ASSET_AUDIO_URLS = {
+  english:  `${ASSET_BASE_URL}/NowhereAudio-English/main/audio`,
+  japanese: `${ASSET_BASE_URL}/NowhereAudio-Japanese/main/audio`,
+  chinese:  `${ASSET_BASE_URL}/NowhereAudio-Chinese/main/audio`,
+  korean:   `${ASSET_BASE_URL}/NowhereAudio-Korean/main/audio`,
+};
 export const ASSET_IMAGES_URL = `${ASSET_BASE_URL}/NowhereImages/main`;
 export const ASSET_PRISON_URL = `${ASSET_IMAGES_URL}/prison`;
 export const ASSET_LOGOS_URL = `${ASSET_IMAGES_URL}/logos`;
